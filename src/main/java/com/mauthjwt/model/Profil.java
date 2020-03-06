8dc816563ef36524e032aab1a3aa6d1acc1544d1
Yassine_TAIR <yassine1tair@gmail.com>
2020-03-06 18:28:50 +0100

    YTR_profil 1

--- src/main/java/com/mauthjwt/model/Profil.java
+++ src/main/java/com/mauthjwt/model/Profil.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
 public class Profil implements Serializable {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into `PROFIL` (CODE_TYPE_USER, LIBELLE) values ('P', 'admin_central'),('U', 'admin_local'),('U', 'utilisateur_normal'),('U', 'utilisateur_premium');
+insert into `USER` (EMAIL,FIRST_NAME,LAST_NAME,ID) values ('test@yopmail.com', 'testF', 'testL', 1);
+insert into `USER_COMPLEMENT` (ALIAS, `PASSWORD`, ID) values ('test', '123456789', 1);
